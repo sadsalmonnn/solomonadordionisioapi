ce77e48141edb14837cbf24b05df74eab20ebcec
Solomon Ador-Dionisio <solomon.adord@gmail.com>
2025-08-14 04:57:36 +0200

    fdsf

--- test.py
+++ test.py
@@ -1,20 +1,5 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-import requests
-
-
-def download_file(url, local_filename):
-    try:
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            with open(local_filename, "wb") as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-        print(f"File downloaded successfully to {local_filename}")
-        return True
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading file: {e}")
-        return False
 
 
 SERVICE_ACCOUNT_FILE = "service_account_cred.json"
@@ -35,12 +20,15 @@ resumefile_dic = (
     .execute()
 )
 
+date_modified = (
+    service.files()
+    .get(fileId="1UnaZwlVdjU4gxs4RSECZl6bKo8gx7P0RvL0p7woCjLA", fields="modifiedTime")
+    .execute()["modifiedTime"]
+)
+
+print(date_modified)
+print(resumefile_dic)
+print("\n")
 download_uri = resumefile_dic["response"]["downloadUri"]
 print(download_uri)
 output_filename = "downloaded_script.pdf"
-
-try:
-    download_file(download_uri, output_filename)
-
-except:
-    print("error downloading file")
