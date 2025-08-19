9e4a3eef61ca8cc0c24d9d39a5fafcf7e27ec9f1
Solomon Ador-Dionisio <solomon.adord@gmail.com>
2025-08-19 02:00:16 +0200

    created personalinfo

---
+++ solomonadordionisioapi/personalinfo/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Hobby(models.Model):
+  title = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.title
+  
+
+class PersonalInfo(models.Model):
+  age = models.IntegerField()
+  address = models.CharField(max_length=200)
+  university = models.CharField(max_length=200)
+  highschool = models.CharField(max_length=200)
+  hobbies = models.ManyToManyField(Hobby, related_name="hobbies", blank=True)
