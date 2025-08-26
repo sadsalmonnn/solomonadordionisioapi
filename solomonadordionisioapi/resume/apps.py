1bf4810e5baad6f31f9bdd0aa29bcc302933bf76
dionis17 <solomon.adordionisio@mail.utoronto.ca>
2025-08-26 11:21:50 -0400

    attempted to fix resume

--- solomonadordionisioapi/resume/apps.py
+++ solomonadordionisioapi/resume/apps.py
@@ -5,9 +5,3 @@ import sys
 class ResumeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "resume"
-
-    def ready(self):
-        from . import resumefetch
-
-        if 'runserver' in sys.argv:
-            resumefetch.fetch_and_update_resume()
