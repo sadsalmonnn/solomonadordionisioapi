4f4a0e20ec0cff56ee3bfe41ad9c8b880bfb6181
dionis17 <solomon.adordionisio@mail.utoronto.ca>
2025-08-27 23:09:47 -0400

    semi fixed api home page

--- solomonadordionisioapi/solomonadordionisioapi/urls.py
+++ solomonadordionisioapi/solomonadordionisioapi/urls.py
@@ -1,31 +1,16 @@
-"""
-URL configuration for solomonadordionisioapi project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-
 from rest_framework.routers import DefaultRouter
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
 from django.urls import include, path
 from django.shortcuts import redirect
+
 from experience.views import ExperienceViewSet
 from link.views import LinkViewSet
 from personalinfo.views import PersonalInfoViewSet
 from project.views import ProjectViewSet
 from resume.views import ResumeViewSet
-from link.views import LinkViewSet
 from tag.views import TagViewSet
 
 
@@ -38,9 +23,25 @@ router.register(r"personalinfo", PersonalInfoViewSet)
 router.register(r"tag", TagViewSet)
 
 
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response({
+        "experience": reverse("experience-list", request=request, format=format),
+        "project": reverse("project-list", request=request, format=format),
+        "resume": reverse("resume-list", request=request, format=format),
+        "links": reverse("link-list", request=request, format=format),
+        "personalinfo": reverse("personalinfo-list", request=request, format=format),
+        "tag": reverse("tag-list", request=request, format=format),
+        "auth_register": reverse("register", request=request, format=format),
+        "auth_login": reverse("login", request=request, format=format),
+        "auth_logout": reverse("logout", request=request, format=format),
+    })
+
+
 urlpatterns = [
     path("", lambda request: redirect("api/", permanent=False)),
     path("admin/", admin.site.urls),
+    path("api/", api_root),
     path("api/", include(router.urls)),
     path("api/auth/", include("userauth.urls")),
 ]
